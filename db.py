3190324216f310291bbe83a3394cd65330aa3935
Joonas Maisala <joonas-maisala@hotmail.com>
2022-11-24 17:01:44 +0900

    Debugging

--- db.py
+++ db.py
@@ -1,11 +1,11 @@
-from os import getenv
+import os
 from app import app
 from flask_sqlalchemy import SQLAlchemy
 
-uri = getenv("DATABASE_URL")  # or other relevant config var
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = uri
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
