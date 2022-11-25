964dc06d8a9003ccf9c8d0815478423ca6ae3acb
Joonas Maisala <joonas-maisala@hotmail.com>
2022-11-25 14:28:35 +0900

    Fly debug

--- db.py
+++ db.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 #if uri.startswith("postgres://"):
 #    uri = uri.replace("postgres://", "postgresql://", 1)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+#app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
